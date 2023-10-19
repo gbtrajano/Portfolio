71bd2973637a2003f2343c8cb9f7382e439be395
gabriel-tr <slashnebulous02@gmail.com>
2023-10-18 21:44:18 -0300

    novo portfolio

---
+++ src/components/ProjectsContainer.jsx
@@ -0,0 +1,18 @@
+const ProjectsContainer = () => {
+  return (
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit non quod
+        doloribus ipsum quaerat illo excepturi quae iure sunt vel quisquam modi,
+        et, doloremque quidem similique quibusdam natus perspiciatis
+        accusantium?
+      </p>
+      <a href="#" className="btn">
+        Ver Projetos
+      </a>
+    </section>
+  );
+};
+
+export default ProjectsContainer;
