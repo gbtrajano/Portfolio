9a0a397b9177d4ed9e350ba9f75be059b2923a02
gbtrajano <gbtrajano1@gmail.com>
2025-05-14 22:30:35 -0300

    ClickSound Change

--- src/app/Projetos/page.tsx
+++ src/app/Projetos/page.tsx
@@ -6,9 +6,8 @@ import "../globals.css";
 import { useState, useEffect } from "react";
 import { useGlobalClickSound } from "../../hooks/useGlobalClickSound";
 
-useGlobalClickSound();
-
 const page = () => {
+  useGlobalClickSound();
   // Definindo o tipo do estado para a posição do cursor
   const [position, setPosition] = useState<{ x: number; y: number }>({
     x: 0,
